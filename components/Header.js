ac7cbbd1a082f17f87b5749b7a2d570f43c336c9
unknown <david.patino1@utp.edu.co>
2024-07-27 10:51:03 -0500

    ajusting header and nav

--- components/Header.js
+++ components/Header.js
@@ -9,24 +9,25 @@ import Socials from '../components/Socials';
 
 const Header = () => {
   return (
-  <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
-    <div className="container mx-auto">
-      <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
-        {/* logo */}
-        <Link href={'/'}>
-          <Image 
-          src={'/logo.png'}
-          width={220}
-          height={48}
-          alt="logo"
-          priority={true}
-          />
-        </Link>
-        {/* socials */}
-        <Socials />
+    <header className="absolute z-30 w-full flex items-center px-4 sm:px-8 lg:px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-4 lg:gap-y-6 py-4 lg:py-8">
+          {/* logo */}
+          <Link href={'/'}>
+            <Image 
+              src={'/logo.png'}
+              width={220}
+              height={48}
+              alt="logo"
+              priority={true}
+              className="w-40 lg:w-56" // Ajustar el tamaño del logo
+            />
+          </Link>
+          {/* socials */}
+          <Socials />
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
   );
 };
 
--- components/Nav.js
+++ components/Nav.js
@@ -20,9 +20,7 @@ export const navData = [
   //   icon: <HiChatBubbleBottomCenterText />,
   // },
   {
-    name: 'contact',
-    path: '/contact',
-    icon: <HiEnvelope />,
+    name: 'contact', path: '/contact', icon: <HiEnvelope />,
   },
 ];
 
@@ -33,47 +31,38 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const Nav = () => {
-
   const router = useRouter();
   const pathname = router.pathname;
 
-  return ( 
-
-  <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
-
-    {/* inner */}
-    <div 
-    className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'
-    >
-
-      {navData.map((link, index) => {
-        return (
-        <Link
-        className={`${link.path === pathname && 'text-accent'} 
-        relative flex items-center group hover:text-accent transition-all duration-250`} 
-        href={link.path} 
-        key={index}
-        > 
-
-        {/* tooltip */}
-        <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
-          <div className='bg-accent relative flex text-primary items-center p-[6px] rounded-[3px]'>
-            <div className='text-[12px] leading-none font-semibold capitalize'>
-              {link.name}
-            </div>
-            {/* triangle */}
-            <div className='border-solid border-l-accent border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'></div>
-          </div>
-        </div>
-
-        {/* icon */}
-        <div>{link.icon}</div>
-        </Link>
-        );
-      })}
-
-    </div>
-  </nav>
+  return (
+    <nav className='flex flex-col items-center xl:justify-center gap-y-6 fixed bottom-0 xl:right-[2%] z-50 w-full xl:w-16 xl:h-screen'>
+      {/* inner */}
+      <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-12 px-6 sm:px-8 md:px-20 lg:px-20 xl:px-0 h-[60px] sm:h-[70px] md:h-[80px] xl:h-auto py-6 xl:py-8 bg-white/10 backdrop-blur-sm text-2xl md:text-3xl xl:text-2xl xl:rounded-full'>
+        {navData.map((link, index) => {
+          return (
+            <Link
+              className={`${link.path === pathname ? 'text-accent' : 'text-white'} 
+              relative flex items-center group hover:text-accent transition-all duration-250`} 
+              href={link.path} 
+              key={index}
+            > 
+              {/* tooltip */}
+              <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
+                <div className='bg-accent relative flex text-primary items-center p-[6px] rounded-[3px]'>
+                  <div className='text-[12px] leading-none font-semibold capitalize'>
+                    {link.name}
+                  </div>
+                  {/* triangle */}
+                  <div className='border-solid border-l-accent border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'></div>
+                </div>
+              </div>
+              {/* icon */}
+              <div>{link.icon}</div>
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
   );
 };
 
