df112c18056dce05180fa217c1e3c45b8af96b41
unknown <david.patino1@utp.edu.co>
2024-08-08 15:54:14 -0500

    opengraph v1.6

--- pages/_app.js
+++ pages/_app.js
@@ -40,7 +40,6 @@ function MyApp({ Component, pageProps }) {
       <meta property="og:type" content="website" />
       <meta property="og:title" content="David Patiño | Full-Stack Developer" />
       <meta property="og:description" content="Portafolio personal de David Esteban Patiño Juspian, desarrollador fullstack." />
-      <meta property="og:image" content="/public/opengraph-image.png" />
       <meta property="og:url" content="https://pat-s-life-portfolio.vercel.app" />
       <meta property="og:site_name" content="David Patiño | Full-Stack Developer" />
       <meta property="og:locale" content="es_ES" />
